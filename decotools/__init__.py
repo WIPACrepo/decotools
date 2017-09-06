d0812d0d35eee3cc503ff4a2ae74766100b04c46
James Bourbeau <jrbourbeau@gmail.com>
2017-09-06 18:31:25 -0500

    Modifies decotools/__init__.py to only import a partial version when being imported from setup.py

--- decotools/__init__.py
+++ decotools/__init__.py
@@ -1,6 +1,19 @@
 
-from .fileio_iOS import get_iOS_files
-from .fileio_android import get_android_files
-from .blob_extraction import extract_blobs, is_hotspot
+import sys
+
+# Check to see if decotools is being imported from the setup.py script
+# If being loaded on setup, don't attempt to import full decotools package
+# only the __version__ needs to be included in this case.
+try:
+    __DECOTOOLS_SETUP__
+except NameError:
+    __DECOTOOLS_SETUP__ = False
+
+if __DECOTOOLS_SETUP__:
+    sys.stderr.write('\n***Partial import of decotools during the build process***\n')
+else:
+    from .fileio_iOS import get_iOS_files
+    from .fileio_android import get_android_files
+    from .blob_extraction import extract_blobs, is_hotspot
 
 __version__ = '0.0.1'
